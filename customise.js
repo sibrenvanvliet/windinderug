7502710e365b8bbf8b2c29e9d3d0ed9f0af0397a
sibrenvanvliet <s.van.vliet.4@student.rug.nl>
2016-10-12 17:26:20 +0200

    Added preview function to customisation page

--- customise.js
+++ customise.js
@@ -17,13 +17,7 @@ function getCookie(cname) {
 	return "";
 }
 
-function confirmCustomisation() {
-	setCookie("logoImage", $("#logoImage").val());
-	setCookie("pageTitle", $("#pageTitle").val());
-	setCookie("backgroundColour", $("#backgroundColour").val());
-	setCookie("textColour", $("#textColour").val());
-	setCookie("linkColour", $("#linkColour").val());
-	
+function discardChanges() {
 	document.location.href = "index.html";
 }
 
@@ -35,17 +29,40 @@ function resetDefaults() {
 	$("#linkColour").val("#337AB7");
 }
 
+function previewCustomisation() {
+	setCookie("logoImage", $("#asdf").val());
+	setCookie("pageTitle", $("#pageTitle").val());
+	setCookie("backgroundColour", $("#backgroundColour").val());
+	setCookie("textColour", $("#textColour").val());
+	setCookie("linkColour", $("#linkColour").val());
+}
+
+function previewCustomisation() {
+	$("#brandTitle").html($("#pageTitle").val());
+	$("#brandLogo").attr("src", $("#logoImage").val());
+	$("body").css("background-color", $("#backgroundColour").val());
+	$(".customisableText").css("color", $("#textColour").val());
+	$(".customisableLink").css("color", $("#linkColour").val());
+	$(".customisableButton").css("color", $("#backgroundColour").val());
+	$(".customisableButton").css("background-color", $("#linkColour").val());
+	$(".customisableButton").css("border-color", $("#linkColour").val());
+}
+
+function confirmCustomisation() {
+	setCookie("logoImage", $("#logoImage").val());
+	setCookie("pageTitle", $("#pageTitle").val());
+	setCookie("backgroundColour", $("#backgroundColour").val());
+	setCookie("textColour", $("#textColour").val());
+	setCookie("linkColour", $("#linkColour").val());
+	
+	document.location.href = "index.html";
+}
+
 $(document).ready(function(){
 	$("#pageTitle").val(getCookie("pageTitle"));
 	$("#logoImage").val(getCookie("logoImage"));
 	$("#backgroundColour").val(getCookie("backgroundColour"));
 	$("#textColour").val(getCookie("textColour"));
 	$("#linkColour").val(getCookie("linkColour"));
+	previewCustomisation();
 }); 
-
-/*
-
-
-console.log(document.cookie);
-console.log(getCookie("pageTitle"));
-*/
